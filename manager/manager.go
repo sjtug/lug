7b7e499cbbff26fe22582f168ead972706da6517
Zheng Lv <lv.zheng.2015@gmail.com>
2016-06-25 21:55:56 +0800

    manager: implement Run(), buggy

--- manager/manager.go
+++ manager/manager.go
@@ -3,7 +3,8 @@ package manager
 
 import (
 	"fmt"
-
+	"strconv"
+	"time"
 	"github.com/op/go-logging"
 	"github.com/sjtug/lug/config"
 	"github.com/sjtug/lug/worker"
@@ -13,17 +14,43 @@ import (
 type Manager struct {
 	config  *config.Config
 	logger  *logging.Logger
-	workers []worker.Worker
+	workers []*worker.Worker
 }
 
 func NewManager(config *config.Config) (*Manager, error) {
-	newManager := Manager{config, logging.MustGetLogger("manager"), []worker.Worker{}}
+	newManager := Manager{config, logging.MustGetLogger("manager"), []*worker.Worker{}}
+	for _, repoConfig := range config.Repos {
+		w, err := worker.NewWorker(&repoConfig)
+		if err != nil {
+			return nil, err
+		}
+		newManager.workers = append(newManager.workers, w)
+	}
 	return &newManager, nil
 }
 
 // Run() will block current routine
 func (m *Manager) Run() {
-
+	c := time.Tick(time.Duration(m.config.Interval) * time.Second)
+	for {
+		// wait until config.Interval seconds has elapsed
+		<-c
+		m.logger.Info("Start polling workers")
+		for _, worker := range m.workers {
+			wStatus := (*worker).GetStatus()
+			if !wStatus.Idle {
+				continue
+			}
+			wConfig := (*worker).GetConfig()
+			elapsed := time.Since(wStatus.LastFinished)
+			sec2sync, _ := strconv.Atoi((*wConfig)["interval"])
+			if elapsed > time.Duration(sec2sync) {
+				m.logger.Noticef("Interval of worker %s (%d sec) elapsed, trigger it to sync", sec2sync)
+				(*worker).TriggerSync()
+			}
+		}
+		m.logger.Info("Stop polling workerks")
+	}
 }
 
 func Foo() {
--- manager/manager_test.go
+++ manager/manager_test.go
@@ -2,13 +2,16 @@ package manager
 
 import (
 	"testing"
-
+	"github.com/op/go-logging"
 	"github.com/sjtug/lug/config"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestManagerStartUp(t *testing.T) {
-	manager, err := NewManager(&config.Config{})
+	manager, err := NewManager(&config.Config{3, logging.INFO,
+		[]config.RepoConfig{
+			config.RepoConfig {"type": "rsync"}}})
 	assert.Nil(t, err)
 	assert.NotNil(t, manager)
+	manager.Run()
 }
