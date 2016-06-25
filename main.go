67876458bbc14a83df03d31048d1087675bff1f9
Vic Luo <vicluo96@gmail.com>
2016-06-25 19:38:32 +0800

    Implemented prototype of main

--- main.go
+++ main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
 	"github.com/sjtug/lug/config"
 	"github.com/sjtug/lug/manager"
 	"github.com/sjtug/lug/worker"
 )
 
+type CommandFlags struct {
+	configFile string
+}
+
+func getFlags() (flags CommandFlags) {
+	flag.StringVar(&flags.configFile, "c", "config.yaml",
+		`Configuration file of lug.
+	Example:
+	interval: 30 # Interval between pollings
+	loglevel: 5 # 1-5
+	repos:
+	 - type: rsync
+	   source: vim.org
+	   interval: 600 # Interval between sync
+	`)
+	flag.Parse()
+
+}
+
 func main() {
-	config.Foo()
-	manager.Foo()
-	worker.Foo()
-	fmt.Printf("Hello world!")
+	flags := getFlags()
+	dat, err := ioutil.ReadFile(flags.configFile)
+	if err != nil {
+		panic(err)
+	}
+
+	cfg := config.Config{}
+	cfg.Parse(dat)
+
 }
