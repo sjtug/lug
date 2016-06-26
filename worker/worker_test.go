97a66da3f58bc6e8a6ae2179ab9c759eb11c7612
hawnzug <hawnzug@gmail.com>
2016-06-26 19:02:18 +0000

    worker: modify test

--- worker/worker_test.go
+++ worker/worker_test.go
@@ -10,6 +10,9 @@ import (
 func TestNewWorker(t *testing.T) {
 	var c config.RepoConfig = make(map[string]string)
 	c["type"] = "rsync"
+	c["name"] = "putty"
+	c["source"] = "source"
+	c["path"] = "path"
 	c["b"] = "bbb"
 
 	assert := assert.New(t)
@@ -18,6 +21,9 @@ func TestNewWorker(t *testing.T) {
 
 	assert.Equal(true, w.GetStatus().Result)
 	assert.Equal("rsync", w.GetConfig()["type"])
+	assert.Equal("putty", w.GetConfig()["name"])
+	assert.Equal("path", w.GetConfig()["path"])
+	assert.Equal("source", w.GetConfig()["source"])
 	assert.Equal("bbb", w.GetConfig()["b"])
 
 }
