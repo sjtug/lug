53dc687d24c17f637af108773f45cf3f4d515455
Vic <vicluo96@gmail.com>
2016-08-29 15:34:01 +0800

    lint

--- helper/max_length_slice.go
+++ helper/max_length_slice.go
@@ -2,13 +2,15 @@ package helper
 
 import "sync"
 
+// MaxLengthStringSliceAdaptor wrap the given slice with several methods for MaxLenSlice.
+// Note that this adaptor does not own that slice at all, so keep the content of slice unchanged!
 type MaxLengthStringSliceAdaptor struct {
 	s      []string
 	maxlen int
 	lock   sync.Mutex
 }
 
-// Create an adaptor for given string slice
+// NewMaxLengthStringSliceAdaptor creates an adaptor for given string slice
 func NewMaxLengthStringSliceAdaptor(s []string, maxlen int) *MaxLengthStringSliceAdaptor {
 	return &MaxLengthStringSliceAdaptor{
 		s:      s,
@@ -16,10 +18,12 @@ func NewMaxLengthStringSliceAdaptor(s []string, maxlen int) *MaxLengthStringSlic
 	}
 }
 
+// MaxLen returns the maximum length of given maxlenslice
 func (m MaxLengthStringSliceAdaptor) MaxLen() int {
 	return m.maxlen
 }
 
+// Put adds a new item into slice, and may remove exceeded item(s)
 func (m *MaxLengthStringSliceAdaptor) Put(str string) {
 	m.lock.Lock()
 	defer m.lock.Unlock()
@@ -29,6 +33,7 @@ func (m *MaxLengthStringSliceAdaptor) Put(str string) {
 	}
 }
 
+// GetAll creates a duplicate of current slice and returns it
 func (m MaxLengthStringSliceAdaptor) GetAll() []string {
 	m.lock.Lock()
 	defer m.lock.Unlock()
@@ -37,13 +42,14 @@ func (m MaxLengthStringSliceAdaptor) GetAll() []string {
 	return result
 }
 
+// Len returns current length of slice
 func (m MaxLengthStringSliceAdaptor) Len() int {
 	m.lock.Lock()
 	defer m.lock.Unlock()
 	return len(m.s)
 }
 
-// Copy and init a new max length string slice
+// NewMaxLengthSlice copies and inits a new max length string slice
 func NewMaxLengthSlice(s []string, maxlen int) *MaxLengthStringSliceAdaptor {
 	news := make([]string, len(s))
 	copy(news, s)
